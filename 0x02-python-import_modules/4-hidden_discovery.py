1a2cee91d514644210f28d6000da33f481d66a19
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-28 02:29:14 +0000

    more adjustments

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == '__main__':
-    newdir = dir(hidden_4)
-    length = len(newdir)
-    for x in range(length):
-        if newdir[x].startswith("__") is True:
+    for x in dir(hidden_4):
+        if x.startswith("__") is True:
             continue
         else:
-            print("{}".format(newdir[x]))
+            print("{}".format(x))
