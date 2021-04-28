dc8b3ff105dbe3e3363f1ae28628e8e53e8189e8
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-28 19:29:57 +0000

    Adding print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 1:
+        print()
+    else:
+        for x in matrix:
+            length = len(x) - 1
+            for y in range(length):
+                print("{:d}".format(x[y]), end=" ")
+            print("{:d}".format(x[length]))
