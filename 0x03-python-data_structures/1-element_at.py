c73ec45f90bb874424b80396d97be3c372fd5d98
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-28 18:08:37 +0000

    updating for loop for range in element_at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
-    for x in my_list:
+    for x in range(len(my_list)):
         if x == idx:
             return my_list[x]
