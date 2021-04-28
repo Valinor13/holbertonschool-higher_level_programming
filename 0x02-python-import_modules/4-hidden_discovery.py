3371be5930c69da1d31262e15f9776256c8a3024
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-28 02:30:56 +0000

    more adjustments

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import hidden_4
 if __name__ == '__main__':
     for x in dir(hidden_4):
-        if x.startswith("__") is True:
+        if x.startswith("__"):
             continue
         else:
             print("{}".format(x))
