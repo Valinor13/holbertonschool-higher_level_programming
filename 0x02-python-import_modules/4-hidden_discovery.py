8efc384fc9517448722fb49e4272c6715a37563f
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-28 02:09:37 +0000

    Adding hidden discovery program

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == '__main__':
-    print(dir(hidden_4))
+    newdir = dir(hidden_4)
+    length = len(newdir)
+    for x in range(length):
+        if newdir[x][0] == '_' and newdir[x][1] == '_':
+            continue
+        else:
+            print(newdir[x])
---
+++ 0x02-python-import_modules/hidden_4.py
@@ -0,0 +1,10 @@
+# File: h (Python 3.4)
+
+def print_holberton():
+    print('holberton')
+
+def print_school():
+    print('school')
+
+def my_secret_santa():
+    return 98
