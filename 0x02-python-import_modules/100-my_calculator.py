4be06d95cba0f66c0018a65ce496c79926e1d3e7
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-28 04:04:30 +0000

    Updating import method and string check for my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        op = sys.argv[2]
-        if op.startswith("+"):
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif op.startswith("-"):
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif op.startswith('*'):
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif op.startswith("/"):
-            print("{} / {} = {}".format(a, b, div(a, b)))
+        a = int(argv[1])
+        b = int(argv[3])
+        op = argv[2]
+        if op == ("+"):
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif op == ("-"):
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif op == ('*'):
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif op == ("/"):
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+            exit(1)
