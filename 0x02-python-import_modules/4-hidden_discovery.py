0147201bcc7d0709d20b05cfed163147efe2b159
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-28 02:25:38 +0000

    updating hidden discovery to use startswith

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     newdir = dir(hidden_4)
     length = len(newdir)
     for x in range(length):
-        if newdir[x][0] == '_' and newdir[x][1] == '_':
+        if newdir[x].startswith("__") == True:
             continue
         else:
             print("{}".format(newdir[x]))
