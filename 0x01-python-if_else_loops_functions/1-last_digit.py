7df035ac336ce3429cb30dd912dfd1f11a552d3f
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-28 04:17:24 +0000

    fixing kerning issues in print statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {:d} is".format(number), end=" ")
+print("Last digit of", number, "is", end=" ")
 if number % 10 == 0:
-    print("{:d} and is 0".format(number % 10))
+    print(number % 10, "and is 0")
 elif number < 0:
     number *= -1
-    print("-{:d} and is less than 6 and not 0".format(number % 10))
+    print("-{:d}".format(number % 10), "and is less than 6 and not 0")
 elif number % 10 > 5:
-    print("{:d} and is greater than 5".format(number % 10))
+    print(number % 10, "and is greater than 5")
 else:
-    print("{:d} and is less than 6 and not 0".format(number % 10))
+    print(number % 10, "and is less than 6 and not 0")
