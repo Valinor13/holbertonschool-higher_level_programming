c63c6954ee6020d3da80cacb29a796317b927b81
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-29 23:11:55 +0000

    Updating multiply list with map to remove for

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return ([list(map(lambda x: x * x, items)) for items in matrix])
+    return list(map(lambda x: list(map(lambda xx: xx ** 2, x)), matrix))
