d43f2d9ef762fbcf47f292826caf688efe0c22b5
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-29 17:39:42 +0000

    Changing fail activity

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    if bool(a_dictionary) is False:
+        return None
     keylist = a_dictionary.keys()
     sig = 0
     for i in keylist:
