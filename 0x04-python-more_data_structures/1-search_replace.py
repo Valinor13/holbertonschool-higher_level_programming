809e2157070d714e072360b8a8e94f3652722dd3
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-29 17:27:05 +0000

    updating search and replace for empty checks

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
+    if not my_list or not search or not replace:
         return None
     newlist = []
     for i in range(len(my_list)):
