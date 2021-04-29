3d3b711ced3e775bc26f10744e06800218b1e07a
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-29 18:27:20 +0000

    adding fail check

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    if bool(a_dictionary) is False:
+        return None
     newdict = a_dictionary.copy()
     for k in newdict:
         newdict[k] *= 2
