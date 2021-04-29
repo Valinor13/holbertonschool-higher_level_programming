f689409dd33f31d1874af94fa75116de72051ede
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-29 17:35:26 +0000

    Changing fail activity

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     if bool(a_dictionary) is False:
-        print()
+        return
     for x in sorted(a_dictionary.keys()):
         print("{}: {}".format(x, a_dictionary[x]))
