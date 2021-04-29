488ed1fb072a00723383e8d1654d23e9d604bd10
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-29 14:01:38 +0000

    Adding common elements module

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,5 @@ def uniq_add(my_list=[]):
     if not my_list:
         return None
     newset = set(my_list)
-    uniqlist = (newset)
-    sumnum = sum(uniqlist)
+    sumnum = sum(newset)
     return sumnum
