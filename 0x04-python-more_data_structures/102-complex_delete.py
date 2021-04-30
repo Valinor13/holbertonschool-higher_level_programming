9603374a2766a2f1b138d55a36bde9464a833c38
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-30 00:17:28 +0000

    Adding complex delete module

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if bool(a_dictionary) is False:
+        return None
+    sig = 1
+    while sig == 1:
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
+            else:
+                sig = 0
+    return a_dictionary
