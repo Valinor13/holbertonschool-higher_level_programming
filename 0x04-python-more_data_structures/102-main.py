2155b3bb4b0d17f5ba5846aed8389eb0abc9f76a
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-30 00:25:43 +0000

    Removing null check

--- 0x04-python-more_data_structures/102-main.py
+++ 0x04-python-more_data_structures/102-main.py
@@ -4,7 +4,7 @@ print_sorted_dictionary = \
             __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
-new_dict = complex_delete(a_dictionary, '')
+new_dict = complex_delete(a_dictionary, 'C')
 print_sorted_dictionary(a_dictionary)
 print("--")
 print_sorted_dictionary(new_dict)
