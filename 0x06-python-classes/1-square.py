affbb9b7b3430f0fa4135d42392e7df926c8ccda
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-04 04:00:34 +0000

    Adding square 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,4 +6,6 @@ class Square:
     """A class to store squares with their size"""
 
     def __init__(self, __size):
+        """Initialization of self and size of square"""
+
         self.__size = __size
