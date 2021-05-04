0a0288834cbb835d2480d882d121876ad2d143f9
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-04 06:05:02 +0000

    updating except checks

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    if not my_list:
-        return 0
     printcount = 0
     loopcount = 0
-    while loopcount < x:
-        if type(my_list[loopcount]) is int:
-            try:
-                print("{:d}".format(my_list[loopcount]), end="")
-                printcount += 1
-            except:
-                print("", end="")
-        else:
-                print("", end="")
+    for loopcount in range(x):
+        try:
+            print("{:d}".format(my_list[loopcount]), end="")
+            printcount += 1
+        except IndexError:
+            break
+        except:
+            continue
         loopcount += 1
     print()
     return printcount
