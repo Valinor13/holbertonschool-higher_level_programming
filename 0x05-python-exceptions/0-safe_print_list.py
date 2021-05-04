279b73a82091203f7e805eb0c8afe9a7188e2b5e
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-04 05:39:43 +0000

    Changing print location

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,12 +5,11 @@ def safe_print_list(my_list=[], x=0):
     while loopcount < x:
         try:
             print("{}".format(my_list[loopcount]), end="")
-            if loopcount == x - 1:
-                print()
             printcount += 1
         except:
             print()
             return printcount
         finally:
             loopcount += 1
+    print()
     return printcount
