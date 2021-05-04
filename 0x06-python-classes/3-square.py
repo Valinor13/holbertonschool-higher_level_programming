6e61ae5d55ee03c27e9a46b29fefb38b5bc2e7a2
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-04 04:29:12 +0000

    Adding square 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module for storing geometric shapes and their attributes"""
+
+
+class Square:
+    """A class to store the square shape and its size safely"""
+
+    def __init__(self, __size=0):
+        """Initialization of self and size of square"""
+
+        self.__size = __size
+
+        if type(__size) is not int:
+            raise TypeError("size must be an integer")
+        if __size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """A function that returns area of square"""
+
+        return self.__size ** 2
