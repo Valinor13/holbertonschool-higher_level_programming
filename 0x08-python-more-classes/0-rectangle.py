99691e049e778a0aaa77cf5cc83c01d9420da080
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-05 19:54:13 +0000

    Adding new directory with rectangle class

---
+++ 0x08-python-more-classes/0-rectangle.py
@@ -0,0 +1,27 @@
+#!/urs/bin/python3
+"""
+A module that holds geometric shapes in classes
+
+...
+
+Classes
+-------
+Rectangle
+    Holds attributes of a rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class used to represent rectangles
+
+    ...
+
+    Methods
+    -------
+    pass
+    """
+
+
+    pass
+    """Pass represents an empty class"""
---
+++ 0x08-python-more-classes/README.md
@@ -0,0 +1,1 @@
+This is a directory for more python class projects in the Holberton higher level programming respository.
