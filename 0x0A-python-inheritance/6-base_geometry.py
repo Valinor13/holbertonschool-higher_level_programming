1311459d06c56535411e07163d23aa63c5b0bbb7
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-11 19:00:32 +0000

    Adding base geometry with exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module containing an class called base geometry"""
+
+
+class BaseGeometry:
+    """A class with a func that raises an exception"""
+
+    def area(self):
+        """A function that raises an exception"""
+
+        raise Exception("area() is not implemented")
