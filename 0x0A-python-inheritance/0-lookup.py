d42ddf5bc8681c01ae24233b955d84254a1d7664
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-11 18:06:02 +0000

    Adding lookup function with new directory

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A module containing a function call to
+lookup object attributes and methods
+"""
+
+
+def lookup(obj):
+
+    return [dir(obj)]
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This directory deals with python class inheritance. It is located in the Holberton higher level programming repository.
