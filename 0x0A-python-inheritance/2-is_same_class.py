514c5a4011bbab7da44697b8be383ecbbef8523a
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-11 18:39:18 +0000

    Updating is is instance funct

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,6 @@
 def is_same_class(obj, a_class):
     """A function that returns True if types match"""
 
-    if a_class is object or a_class is bool:
+    if a_class is object or a_class is True or a_class is False:
         return False
     return isinstance(obj, a_class)
