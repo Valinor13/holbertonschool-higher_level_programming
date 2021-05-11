4bb3d7ce068a119343b4eb5f92509a3b85f65794
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-11 19:50:17 +0000

    Updating is same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,4 @@
 def is_same_class(obj, a_class):
     """A function that returns True if types match"""
 
-    if a_class is object or isinstance(obj, bool):
-        return False
-    return isinstance(obj, a_class)
+    return isinstance(type(obj), a_class)
