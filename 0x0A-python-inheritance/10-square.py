b7dddd687ffc281d2899f4fa5585b9f813730728
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-11 20:19:15 +0000

    Fixing imports

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,41 +1,9 @@
 #!/usr/bin/python3
-"""A module containing classes dealing with geometric shapes"""
+"""A module containing the square class inherited from Rectangle"""
 
 
-class BaseGeometry:
-    """A class with a func that raises an exception"""
+Rectangle = __import__('9-rectangle').Rectangle
 
-    def area(self):
-        """A function that raises an exception"""
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """A function that validates the value parameter"""
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """A class with functions that handle arguments for a rectangle"""
-
-    def __init__(self, width, height):
-
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-
-        return self.__width * self.__height
-
-    def __str__(self):
-
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
 class Square(Rectangle):
     """A class inherited from rectangle that deals with squares"""
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
-"""A module containing an class called Rectangle"""
+"""A module containing Rectangle inherited from BaseGeometry"""
 
 
-class BaseGeometry:
-    """A class with a func that raises an exception"""
-
-    def area(self):
-        """A function that raises an exception"""
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """A function that validates the value parameter"""
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
