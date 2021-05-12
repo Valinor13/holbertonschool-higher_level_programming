eb43b45501a0f3968ec11fbe10d59369922b89bf
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-12 18:37:15 +0000

    Trying something different

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ stjf = __import__('5-save_to_json_file').save_to_json_file
 lfjf = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-my_list = [lfjf(filename)]
+my_list = lfjf(filename)
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
 stjf(my_list, filename)
