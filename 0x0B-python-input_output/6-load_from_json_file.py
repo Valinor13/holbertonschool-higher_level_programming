d36c6274529fec5f440294300f979e60861ee324
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-12 17:36:25 +0000

    Adding load from json file func

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module with a function to load a json file and convert to python"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """A function that loads a json file as python"""
+
+    with open(filename) as f:
+        string = json.loads("".join(f.readlines()))
+        f.close()
+
+    return string
