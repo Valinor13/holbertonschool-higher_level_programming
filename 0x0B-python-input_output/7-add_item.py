c60607236a8869644d8f48e346ba8715695b66d0
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-12 19:22:44 +0000

    Updating add item func

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,14 +8,24 @@ import sys
 
 stjf = __import__('5-save_to_json_file').save_to_json_file
 lfjf = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
 
-filename = "add_item.json"
-try:
-    f = open(filename, "x")
-    f.close()
-except:
-    pass
-my_list = [lfjf(filename)]
-for i in range(1, len(sys.argv)):
-    my_list.append(str(sys.argv[i]))
-stjf(my_list, filename)
+    my_list = []
+    my_string = ""
+    filename = "add_item.json"
+    try:
+        f = open(filename, "x")
+        f.close()
+        f = open(filename, "w")
+        f.write(my_string)
+        f.close()
+    except:
+        with open(filename) as f:
+            if f.seek(0) is "":
+                f.close()
+            else:
+                my_string = lfjf(filename)
+    my_list.append([my_string])
+    for i in range(1, len(sys.argv)):
+        my_list.append(str(sys.argv[i]))
+    stjf(my_list, filename)
