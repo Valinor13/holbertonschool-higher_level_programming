5ffd055d0b0c031a6ffe3c08a40b8a5332511630
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-12 16:14:14 +0000

    Adding new directory with read file func

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module that stores a function built to read a text file"""
+
+
+def read_file(filename=""):
+    """A function to read a text file"""
+
+    with open(filename) as f:
+        print("".join(f.readlines()), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This directory includes python input/output files. It is located in the Holberton higher level programming repository.
