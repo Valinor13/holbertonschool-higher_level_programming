aaf60b45b5a6989ba3999865b5680f733ef3b74f
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-13 01:47:14 +0000

    Adding student 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A module with the class Student"""
+
+
+class Student:
+    """A class that stores student with their records"""
+
+    def __init__(self, first_name, last_name, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        a = {}
+        if attrs is None:
+            return vars(self)
+        if type(attrs) is str:
+            try:
+                a[attrs] = getattr(self, attrs)
+                return a
+            except:
+                pass
+        for item in attrs:
+            try:
+                a[item] = getattr(self, item)
+            except:
+                pass
+        return a
+
+    def reload_from_json(self, json):
+
+        for k, v in json:
+            setattr(self, k, v)
