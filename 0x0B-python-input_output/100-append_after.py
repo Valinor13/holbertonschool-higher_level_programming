94cc57315da496ba37cb4eb659d6188c52e68959
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-13 04:18:40 +0000

    Fixing append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,12 +5,16 @@
 def append_after(filename="", search_string="", new_string=""):
     """A function to append to a file in a certain location"""
 
-    with open(filename, "w+") as f:
-        my_list = f.readlines()
-        my_string = ""
-        for item in my_list:
-            my_string += item
-            if item is search_string:
-                my_string += new_string
-        f.write(my_string)
+    with open(filename) as f:
+        read_line = f.readline()
+        read_string = ""
+        while read_line:
+            if search_string in read_line:
+                read_string += read_line + new_string
+            else:
+                read_string += read_line
+            read_line = f.readline()
+        f.close()
+    with open(filename, "w") as f:
+        f.write(read_string)
         f.close()
