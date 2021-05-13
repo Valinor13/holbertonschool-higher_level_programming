9e830f05e767a0c69ce73635b3b8398fae5be98b
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-13 03:02:35 +0000

    Adding pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,15 +7,13 @@ def pascal_triangle(n):
 
     my_matrix = []
     if n <= 0:
-        return my_list
+        return my_matrix
     for i in range(n):
         row_list = []
         for j in range(i + 1):
-            if j == i:
-                row_list.append(1)
-                my_matrix.append(1)
-            elif j == 0:
+            if j == 0 or j == i:
                 row_list.append(1)
             else:
-                row_list.append(my_matrix[i - 1][j - 1] + my_matrix[i - 1][j])
-    return my_list
+                row_list.append(my_matrix[i - 1][j -  1] + my_matrix[i - 1][j])
+        my_matrix.append(row_list)
+    return my_matrix
