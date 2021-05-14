fb43626e05468f47796c76f794b7da916a75a20e
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-14 16:13:28 +0000

    Updating getter and setter functions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,55 +12,55 @@ class Rectangle(Base):
         """The Rectangle constructor function"""
 
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
         """The width getter function"""
 
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """The width setter function"""
 
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
         """The height getter function"""
 
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """The height setter function"""
 
-        self.height = value
+        self.__height = value
 
     @property
     def x(self):
         """The x getter function"""
 
-        return self.x
+        return self.__x
 
     @x.setter
     def x(self, value):
         """The x setter function"""
 
-        self.x = x
+        self.__x = value
 
     @property
     def y(self):
         """The y getter function"""
 
-        return self.y
+        return self.__y
 
     @y.setter
     def y(self, value):
         """The y setter function"""
 
-        self.y = y
+        self.__y = value
