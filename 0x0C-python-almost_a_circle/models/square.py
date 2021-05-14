f58274c2bba7a3ba52a11ab4cf44df242441a542
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-14 18:44:20 +0000

    Adding a non working getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,9 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """The square constructor class"""
 
+        self.size = size
         super().__init__(size, size, x, y, id)
+
 
     def __str__(self):
         """A function to overload __str__"""
@@ -19,3 +21,19 @@ class Square(Rectangle):
         s = "[Square] ({}) {}/".format(self.id, self.x)
         s += "{} - {}".format(self.id, self.x)
         return s
+
+    @property
+    def size(self):
+        """The size getter function"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """The size setter function"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = value
