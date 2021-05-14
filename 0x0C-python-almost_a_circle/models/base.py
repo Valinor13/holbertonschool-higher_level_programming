4508141f9bf603d74861a1f96b462674b423a8d8
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-14 21:55:37 +0000

    Adding list of dictionaries check to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """A module containing the class Base"""
 
 
+import json
+
+
 class Base:
     """The class Base"""
 
@@ -15,3 +18,13 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """A function that converts a dictionary to a json string"""
+
+        if list_dictionaries is None or list_dictionaries[0] is {}:
+            return "[]"
+        my_string = ""
+        for item in list_dictionaries:
+            my_string += json.dumps(list_dictionaries)
+        return my_string
