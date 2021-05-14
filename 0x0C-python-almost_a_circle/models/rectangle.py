cde672ed9dc99414330183c23592c74412e0bc32
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-14 16:02:19 +0000

    Adding getters and setters back

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,3 +16,51 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        """The width getter function"""
+
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        """The width setter function"""
+
+        self.width = value
+
+    @property
+    def height(self):
+        """The height getter function"""
+
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        """The height setter function"""
+
+        self.height = value
+
+    @property
+    def x(self):
+        """The x getter function"""
+
+        return self.x
+
+    @x.setter
+    def x(self, value):
+        """The x setter function"""
+
+        self.x = x
+
+    @property
+    def y(self):
+        """The y getter function"""
+
+        return self.y
+
+    @y.setter
+    def y(self, value):
+        """The y setter function"""
+
+        self.y = y
