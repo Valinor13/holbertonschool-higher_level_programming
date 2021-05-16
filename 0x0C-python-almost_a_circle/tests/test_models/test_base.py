df81fd3781afdd41a11bcb6c15f48ad430518d0c
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-16 21:12:18 +0000

    Adding tests to base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -13,11 +13,17 @@ class TestBaseClass(unittest.TestCase):
 
     def test_auto_id(self):
         b1 = Base()
-        b2 = Base(None)
-        b3 = Base(89)
         self.assertEqual(b1.id, 1)
+        b2 = Base(None)
         self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 89)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+
+    def test_id_match(self):
+        b1 = Base(1)
+        self.assertEqual(b1.id, 1)
+        b2 = Base(89)
+        self.assertEqual(b2.id, 89)
 
 if __name__ == '__main__':
     unittest.main()
