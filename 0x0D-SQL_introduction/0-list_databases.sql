7d2371f74e03ccf7b345411d980184e8805b3943
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-26 21:39:30 +0000

    Adding list databases in sql server

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- 3 first students in the Batch ID=3
+-- because Cohort 14a is the best!
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,1 @@
+This is the SQL introduction in the Holberton higher level programming repository.
