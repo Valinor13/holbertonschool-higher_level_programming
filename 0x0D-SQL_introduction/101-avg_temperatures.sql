e28fd833626a467f333e46c9f2bc02ae3af7a813
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-27 00:26:53 +0000

    Changing the value of values

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Displays the avg temp per city.
-SELECT city, AVG(temperatures) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
