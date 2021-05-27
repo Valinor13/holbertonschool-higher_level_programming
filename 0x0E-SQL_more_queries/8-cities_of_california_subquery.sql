ca4ae417c63659299cae8044fabe43477eef7890
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-27 20:42:22 +0000

    Fixing depth of cities in CA

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 -- Lists all cities of California in database
 
-SELECT id, name FROM cities WHERE name = 'California' ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id IN(
+	SELECT id, name FROM states WHERE name = 'California') ORDER BY cities.id ASC;
