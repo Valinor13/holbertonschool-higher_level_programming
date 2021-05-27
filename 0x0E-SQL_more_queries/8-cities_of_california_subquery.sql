14ed0eefd872c3374197c9d4c637ea6e4ba22910
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-27 20:36:54 +0000

    updating cities of CA

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Lists all cities of California in database
 
-SELECT id, name FROM cities WHERE name=California ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE name = 'California' ORDER BY cities.id ASC;
