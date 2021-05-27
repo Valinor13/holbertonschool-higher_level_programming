84d59bd7e8ce9ca438bfca62ca555f2d5dda3e87
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-27 00:25:34 +0000

    Fixing syntax error

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Displays the avg temp per city.
-SELECT city, AVG(temperatures) AS avg_temp FROM hbtn_0c_0 GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(temperatures) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
