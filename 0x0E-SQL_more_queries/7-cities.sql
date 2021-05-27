6785f2e138d098fbac592317b4ff7cbff64de92b
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-27 20:24:36 +0000

    Updating cities references

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	UNIQUE (ID),
 	PRIMARY KEY (ID),
 	state_id INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id),
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL
 );
