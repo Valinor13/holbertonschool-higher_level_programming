097912997934d95befba4e9f6e80bf195f4fa341
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-28 02:18:57 +0000

    adding genre id of all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows in database
+
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY
+title, genre_id ASC;
