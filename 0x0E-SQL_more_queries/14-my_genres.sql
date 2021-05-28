76cede0ba7824b4782da5ccc06cd7fef6fc9c598
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-28 03:14:51 +0000

    Adding my genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- Lists all genres of the show Dexter
+
+SELECT name
+FROM tv_shows ts
+INNER JOIN tv_show_genres tsg
+ON ts.id = tsg.show_id
+INNER JOIN tv_genres tg
+ON tsg.genre_id = tg.id
+WHERE ts.title = 'Dexter'
+ORDER BY tg.name;
