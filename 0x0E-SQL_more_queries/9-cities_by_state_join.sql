fc13797e8de062221670227335fcd342897db60b
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-28 00:59:56 +0000

    Updating cities by state join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,9 @@
 -- Lists all cities contained in database.
 -- hbtn_0d_usa
 
-SELECT cities.id, cities.name, states.name
-FROM cities
-FULL OUTER JOIN states ON cities.id=states.id 
-ORDER BY cities.id ASC;
+SELECT *
+FROM states
+INNER JOIN cities
+ON cities.id = states.id 
+ORDER BY
+cities.id ASC;
