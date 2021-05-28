caf929d64beca19c724b6b199719acf6b9c73b48
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-28 01:41:27 +0000

    Updating states, still doesn't run locally

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,9 @@
 -- Creates a database and table.
--- hbtn_0d_usa, states(id, name)
+-- hbtn_0d_usa, states
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-	id INT NOT NULL AUTO_INCREMENT, 
-	UNIQUE (ID),
-	PRIMARY KEY (ID),
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
