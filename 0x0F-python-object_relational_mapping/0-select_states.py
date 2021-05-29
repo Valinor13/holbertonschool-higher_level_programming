7401eff4afa952fcfd67d747e2f7ca96e90b8387
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-29 21:50:10 +0000

    Adding select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" """
+
+
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3])
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+
+data = cursor.fetchall()
+
+for item in data:
+    print(item)
+
+db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+This is the python object relational mapping directory in the Holberton higher level programming repository.
