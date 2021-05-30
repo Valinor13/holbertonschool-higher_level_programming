4f58336850f8f679d96fa3bbe03643098d30778c
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-30 00:11:55 +0000

    adding model state fetch all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
-from model_state import Base, State
+"""Start link class to table in database"""
 
-from sqlalchemy import (create_engine)
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
 
 if __name__ == "__main__":
     engine = create_engine(
@@ -12,3 +14,12 @@ if __name__ == "__main__":
                                                     sys.argv[2],
                                                     sys.argv[3]),
         pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State)
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
