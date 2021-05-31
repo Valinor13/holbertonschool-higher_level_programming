9f76f1e9b0948d344256996eaf0fda12a8e1ad22
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-31 19:26:16 +0000

    Adding model state my get

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Create a session for database access"""
+
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3]),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State)
+
+    for state in states:
+        if state.name == sys.argv[4]:
+            print(state.id)
+            sig = 0
+            break
+        else:
+            sig = 1
+    if sig == 1:
+        print("Not found")
