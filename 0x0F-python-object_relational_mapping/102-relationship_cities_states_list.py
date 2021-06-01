24a57d65295bfd89d574d0f2296d4a68b4c027da
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-01 02:33:09 +0000

    Adding relationship cities to states

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Create a session for database access"""
+
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+from model_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3]),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for state, city in session.query(State, City):
+        if city.state_id == state.id:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
