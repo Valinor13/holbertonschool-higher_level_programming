10b96bb530926b8821dd241bd3e25afd928ce120
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-03 23:13:25 +0000

    Fixing semi standard

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  con_num = function doCon (num) {
+  const conNum = function doCon (num) {
     return num.toString(base);
-  }
-  return con_num;
-}
+  };
+  return conNum;
+};
