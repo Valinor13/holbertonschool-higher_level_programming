e937499e159a089f84ad95959f8fb7e73f8ecfe9
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-03 01:12:20 +0000

    Adding find second largest number in a list

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+if (process.argv.length < 4) {
+  console.log('0');
+} else {
+  const args = process.argv.slice(2);
+  console.log(args.sort(function (a, b) { return b - a; })[1]);
+}
